8e70eac0aa01829363d6e031e0cc81072de1b816
JerryLiuRutgers <jerry.liu@rutgers.edu>
2016-10-29 23:25:33 -0400

    Created basic functionality and scene layout

---
+++ Assets/B3BehaviorTree.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using TreeSharpPlus;
+
+public class B3BehaviorTree : MonoBehaviour {
+
+    public GameObject fighter1;
+    public GameObject fighter2;
+
+    private BehaviorAgent behaviorAgent;
+
+	void Start () {
+        behaviorAgent = new BehaviorAgent(this.BuildTreeRoot());
+        BehaviorManager.Instance.Register(behaviorAgent);
+        behaviorAgent.StartBehavior();
+	}
+	
+	
+	void Update () {
+	
+	}
+
+    protected Node BuildTreeRoot()
+    {
+        Node fight = new Sequence(
+            fighter2.GetComponent<BehaviorMecanim>().Node_GoTo(fighter1.transform.position),
+            fighter1.GetComponent<BehaviorMecanim>().Node_BodyAnimation("Breakdance", true));
+        return fight;
+    }
+}
