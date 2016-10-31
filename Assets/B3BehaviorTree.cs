2a1559955a45ab7d338847610b67468474aaa6a1
JerryLiuRutgers <jerry.liu@rutgers.edu>
2016-10-30 22:04:44 -0400

    Pretty much done

--- Assets/B3BehaviorTree.cs
+++ Assets/B3BehaviorTree.cs
@@ -5,12 +5,17 @@ using TreeSharpPlus;
 
 public class B3BehaviorTree : MonoBehaviour {
 
-    public GameObject fighter1;
-    public GameObject fighter2;
+    public GameObject protag;
+    public GameObject judge, judge2, judge3, judge4;
+    public GameObject door1;
+    public GameObject door2;
+    public Transform exit;
 
     private BehaviorAgent behaviorAgent;
+    private Boolean doors;
 
 	void Start () {
+        doors = false;
         behaviorAgent = new BehaviorAgent(this.BuildTreeRoot());
         BehaviorManager.Instance.Register(behaviorAgent);
         behaviorAgent.StartBehavior();
@@ -18,14 +23,81 @@ public class B3BehaviorTree : MonoBehaviour {
 	
 	
 	void Update () {
-	
+	    if (Input.GetKeyDown(KeyCode.Space) && doors == false)
+        {
+            OpenDoors();
+        }
 	}
+
+    void OpenDoors()
+    {
+        door1.transform.Translate(new Vector3(-3, 0, 0));
+        door2.transform.Translate(new Vector3(3, 0, 0));
+        doors = true;
+    }
+
+    protected Node ApproachOrient(GameObject approachee, GameObject approacher)
+    {
+        return new Sequence(
+            approacher.GetComponent<BehaviorMecanim>().Node_GoToUpToRadius(approachee.transform.position, 3.0f),
+            approacher.GetComponent<BehaviorMecanim>().Node_OrientTowards(approachee.transform.position),
+            approachee.GetComponent<BehaviorMecanim>().Node_OrientTowards(approacher.transform.position));        
+    }
+
+    protected Node RandomDance(GameObject dancer, GameObject judge)
+    {
+        return new Sequence(
+            new SequenceParallel(
+                new RandomNode(
+                    SatNightDance(dancer),
+                    ApplePick(dancer),
+                    BreakDance(dancer)),
+                judge.GetComponent<BehaviorMecanim>().Node_FaceAnimation("HeadShake", true)),
+            judge.GetComponent<BehaviorMecanim>().Node_BodyAnimation("Breakdance", true));
+    }
+
+    protected Node BreakDance(GameObject dancer)
+    {
+        return new Sequence(
+            dancer.GetComponent<BehaviorMecanim>().Node_BodyAnimation("Breakdance", true),
+            new LeafWait(3000),
+            dancer.GetComponent<BehaviorMecanim>().Node_BodyAnimation("Breakdance", false));
+    }
+
+    protected Node SatNightDance(GameObject dancer)
+    {
+        return new Sequence(
+            dancer.GetComponent<BehaviorMecanim>().Node_HandAnimation("SatNightFever", true),
+            new LeafWait(3000),
+            dancer.GetComponent<BehaviorMecanim>().Node_HandAnimation("SatNightFever", false));
+    }
+
+    protected Node ApplePick(GameObject dancer)
+    {
+        return new Sequence(
+            dancer.GetComponent<BehaviorMecanim>().Node_HandAnimation("ApplePick", true),
+            new LeafWait(4000),
+            dancer.GetComponent<BehaviorMecanim>().Node_HandAnimation("ApplePick", false));
+    }
+
+    protected Node DanceOff()
+    {
+        return new Sequence(
+            ApproachOrient(protag, judge),
+            RandomDance(protag, judge),
+            ApproachOrient(protag, judge2),
+            RandomDance(protag, judge2),
+            ApproachOrient(protag, judge3),
+            RandomDance(protag, judge3),
+            ApproachOrient(protag, judge4),
+            RandomDance(protag, judge4));
+    }
 
     protected Node BuildTreeRoot()
     {
-        Node fight = new Sequence(
-            fighter2.GetComponent<BehaviorMecanim>().Node_GoTo(fighter1.transform.position),
-            fighter1.GetComponent<BehaviorMecanim>().Node_BodyAnimation("Breakdance", true));
-        return fight;
+        Node story = new Sequence(
+            DanceOff(),
+            protag.GetComponent<BehaviorMecanim>().Node_GoTo(exit.position));
+        return story;
     }
 }
---
+++ Assets/Core/Scripts/Behavior/TreeSharpPlus/RandomNode.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TreeSharpPlus
+{
+    public class RandomNode : NodeGroupWeighted
+    {
+        public RandomNode(params NodeWeight[] weightedchildren)
+            : base(weightedchildren)
+        {
+        }
+
+        public RandomNode(params Node[] children)
+            : base(children)
+        {
+        }
+
+        public override void Start()
+        {
+            base.Start();
+            this.Shuffle();
+        }
+
+        public override IEnumerable<RunStatus> Execute()
+        {
+            // Proceed as we do with the original selector
+
+                Node node = this.Children[0];
+                
+                // Move to the next node
+                this.Selection = node;
+                node.Start();
+
+                // If the current node is still running, report that. Don't 'break' the enumerator
+                RunStatus result;
+                while ((result = this.TickNode(node)) == RunStatus.Running)
+                    yield return RunStatus.Running;
+
+                // Call Stop to allow the node to clean anything up.
+                node.Stop();
+
+                // Clear the selection
+                this.Selection.ClearLastStatus();
+                this.Selection = null;
+
+            // If it succeeded, we return success without trying any subsequent nodes
+            if (result == RunStatus.Success)
+            {
+                yield return RunStatus.Success;
+                yield break;
+            }
+            else
+            {
+                // We ran out of children, and none succeeded. Return failed.
+                yield return RunStatus.Failure;
+                // Make sure we tell our parent composite, that we're finished.
+                yield break;
+            }
+        }
+    }
+}
